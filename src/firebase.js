f9d5d4f281f792f24051544ab83e109c2488046d
amarpsp10 <amarpsp10@gmail.com>
2021-05-22 07:14:04 -0700

    deleted firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@
 import firebase from 'firebase'
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyCfFy36pxGEWs1s7qhxCiD2-oD79dpXJYM",
-    authDomain: "to-do-app-50f01.firebaseapp.com",
-    projectId: "to-do-app-50f01",
-    storageBucket: "to-do-app-50f01.appspot.com",
-    messagingSenderId: "718919010590",
-    appId: "1:718919010590:web:527ef7f89a0cfbfcff610f",
-    measurementId: "G-N8XHVQNPQZ"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 })
 
 const db = firebase.firestore();
